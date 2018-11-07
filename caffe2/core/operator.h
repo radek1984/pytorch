b6d98a7bd8d1db9676e053f1a5d3a4465a1019b7
Radek Kintop <r.kintop@samsung.com>
2018-11-07 18:35:10 +0100

    Necessary resisters declarations

--- caffe2/core/operator.h
+++ caffe2/core/operator.h
@@ -896,6 +896,27 @@ C10_DECLARE_REGISTRY(
 #define REGISTER_CUDNN_OPERATOR(name, ...) \
   REGISTER_CUDA_OPERATOR_WITH_ENGINE(name, CUDNN, __VA_ARGS__)
 
+////////////////OPENCL////
+C10_DECLARE_REGISTRY(
+    OpenCLOperatorRegistry,
+    OperatorBase,
+    const OperatorDef&,
+    Workspace*);
+#define REGISTER_OPENCL_OPERATOR_CREATOR(key, ...) \
+  C10_REGISTER_CREATOR(OpenCLOperatorRegistry, key, __VA_ARGS__)
+#define REGISTER_OPENCL_OPERATOR(name, ...)                           \
+  C10_IMPORT void CAFFE2_PLEASE_ADD_OPERATOR_SCHEMA_FOR_##name();   \
+  static void CAFFE2_UNUSED CAFFE_ANONYMOUS_VARIABLE_OPENCL##name() { \
+    CAFFE2_PLEASE_ADD_OPERATOR_SCHEMA_FOR_##name();                 \
+  }                                                                 \
+  C10_REGISTER_CLASS(OpenCLOperatorRegistry, name, __VA_ARGS__)
+#define REGISTER_OPENCL_OPERATOR_STR(str_name, ...) \
+  C10_REGISTER_TYPED_CLASS(OpenCLOperatorRegistry, str_name, __VA_ARGS__)
+
+#define REGISTER_OPENCL_OPERATOR_WITH_ENGINE(name, engine, ...) \
+  C10_REGISTER_CLASS(OpenCLOperatorRegistry, name##_ENGINE_##engine, __VA_ARGS__)
+////////////////////////////
+
 // Macros for HIP operators
 C10_DECLARE_REGISTRY(
     HIPOperatorRegistry,
